1a2866d560d41b73bef48c5346fb5e9b697f0f33
dinesh1 <rokkystar03@gmail.com>
2021-05-10 08:41:18 +0530

    souce code adding

---
+++ src/nariproject/Nariclass.java
@@ -0,0 +1,11 @@
+package nariproject;
+
+public class Nariclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("good morning");
+	}
+
+}
